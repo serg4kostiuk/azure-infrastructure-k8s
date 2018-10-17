d9c954936b06437875fb1684cfff4edd1de7178d
pavlomysiuk <pavlomysiuk@gmail.com>
2018-10-17 19:20:21 +0300

    to create cluster

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "app_id"{
 #description = "appid from az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/{yours subscriptions}""
- default      = "5acc41ea-4b41-4556-a790-38874982b318"
+ default      = "xxxx"
 }
 variable "client_password"{
 # description = "password from az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/{yours subscriptions}"
- default      = "4d640381-c990-4d74-b42b-b37737a65178"
+ default      = "XXX"
 }
 
