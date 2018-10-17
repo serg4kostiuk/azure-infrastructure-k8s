1ad9734a8297bac2256a72200f0513ba268910d0
pavlomysiuk <pavlomysiuk@gmail.com>
2018-10-17 18:06:31 +0300

    tf file to create cluster

---
+++ k8s.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "myk8s" {
+  name     = "myrg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "myk8s" {
+  name                = "myk8s"
+  location            = "${azurerm_resource_group.myk8s.location}"
+  resource_group_name = "${azurerm_resource_group.myk8s.name}"
+  dns_prefix          = "acctestagent1"
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 1
+    vm_size         = "Standard_F1s"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = "${var.app_id}"
+    client_secret = "${var.client_password}"
+  }
+
+  tags {
+    Environment = "Production"
+  }
+}
+
+
