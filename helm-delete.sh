053cac6f92fa69d7f90c99f6a782928a31a07a65
Sergii Kostiuk <qa.onpr@admiralmarkets.com>
2018-10-30 13:29:27 +0200

    leak of cores

--- helm-delete.sh
+++ helm-delete.sh
@@ -2,4 +2,8 @@
 
 helm list
 helm delete $(helm list)
+kubectl get all --namespace kube-system
+kubectl delete --namespace kube-system svc tiller-deploy
+kubectl delete --namespace kube-system deploy tiller-deploy
+helm reset --force
 helm list
--- start.sh
+++ start.sh
@@ -5,16 +5,20 @@ terraform apply -auto-approve
 #clear config file
 truncate -s 0 /home/serg/.kube/config
 az aks get-credentials --resource-group myrg --name myk8s
+alias k='kubectl'
+alias kget='kubectl get --all-namespaces'
 
-#create nodes
+echo "Deploying namespace"
 kubectl apply -f yml/my-namespace.yml
+
+echo "Deploying galera cluster"
+kubectl apply -f yml/etcd-cluster.yml
+kubectl apply -f yml/mariadb-rs.yml
+kubectl apply -f yml/mariadb-pvc.yml
+kubectl apply -f yml/mariadb-ss.yml
+
+echo "Deploying PHP and Apache web server"
 kubectl apply -f yml/php-apache.yml
-kubectl apply -f yml/mysql.yml
-#/bin/bash scripts/create-prometheus.sh
-#kubectl create -f /yml/monitor.yml
 
-#get info
-kubectl get svc,rc,pods -o wide,nods -o wide
-kubectl pods --output=wide
-kubectl nods --output=wide
-
+echo "Get info about deployments"
+watch kubectl get svc,rc,pods -o wide,nods -o wide
